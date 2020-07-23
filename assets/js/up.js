7aef979a2b5cc1ca9f527bfb015db5fd35d88b83
Sebastian Ruiz <sebastian.ruiz8@udea.edu.co>
2020-07-23 17:33:01 -0500

    deleted console log

--- assets/js/up.js
+++ assets/js/up.js
@@ -5,7 +5,6 @@ window.addEventListener("scroll", () => {
   if (window.scrollY > 200) {
     goup.style["opacity"] = 1;
     goup.style["visibility"] = "visible";
-    console.log("agsd");
   } else {
     goup.style["opacity"] = 0;
     goup.style["visibility"] = "hidden";
