2451f130e72ab5d9ad4a7c442ab263fbcb9d5679
Sebastian Ruiz <sebastian.ruiz8@udea.edu.co>
2020-07-23 16:31:56 -0500

    Almost finished

---
+++ assets/js/up.js
@@ -0,0 +1,17 @@
+import jump from "../../node_modules/jump.js/dist/jump.module.js";
+const goup = document.querySelector(".arriba");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 200) {
+    goup.style["opacity"] = 1;
+    goup.style["visibility"] = "visible";
+    console.log("agsd");
+  } else {
+    goup.style["opacity"] = 0;
+    goup.style["visibility"] = "hidden";
+  }
+});
+
+goup.addEventListener("click", () => {
+  jump("#header-wrapper");
+});
